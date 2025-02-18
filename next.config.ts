d171bfcb76cb6bc1ef117d1e0990cc7a8c107016
Jitendra Bakoria <jitendra.desuntechnology@gmail.com>
2025-02-18 16:51:40 +0530

    ignore-eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false, // Disable strict mode properly
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
